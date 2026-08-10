070f1b8712c0e088e3afea331f2e4ca2fd998477
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-10 07:29:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let xp = 0;
+let health =100;
+let gold = 50;
+let current_weapon_index = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
 
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xp_text = document.querySelector("#xpText");
+const health_text = document.querySelector("#healthText");
+const gold_text = document.querySelector("#goldText");
+const monster_stats = document.querySelector("#monsterStats");
+const monster_name = document.querySelector("#monsterName");
+
+const monster_health = document.querySelector("#monster_health");
+const monster_health_text = document.querySelector("#monster_health_text");
